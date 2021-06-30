d730dbcf84f2b35123c149768421e8a83fca76ea
cabejackson <jacksoncaleb.calebjackson@gmail.com>
2021-06-30 14:01:06 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+//import react and reactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+if (module.hot) {
+    module.hot.accept();
+}
+// create a react component
+const App = () => {
+    return <div>Hi There!!</div>
+};
+//take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
